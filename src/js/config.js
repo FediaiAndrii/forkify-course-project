33b2e6a1fe7ddfda6b39cd2243416f1d2fb4e8f8
fediaiandrii <lezukokitagava@gmail.com>
2026-02-16 14:50:24 +0200

    fix icons bug

--- src/js/config.js
+++ src/js/config.js
@@ -3,4 +3,4 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '5f1b0a88-16d5-4523-b1da-358055018656';
 export const MODAL_CLOSE_SEC = 2.5;
-export const ICONS = new URL('../img/icons.svg', import.meta.url).pathname;
+export const ICONS = new URL('../img/icons.svg', import.meta.url).href;
